3c80374100322939370758cc1d912fbd86e50aec
Carson Black <uhhadd@gmail.com>
2019-08-23 16:19:26 -0400

    Added zsh/setup_zsh.sh.

---
+++ zsh/setup_zsh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+CLEAR=$(tput sgr0)
+MAGENTA=$(tput setaf 5)
+
+[[ ! -a /usr/local/bin/antibody ]] && curl -sfL git.io/antibody | sh -s - -b /usr/local/sbin
+
+curl -sfL https://raw.githubusercontent.com/Appadeia/dotfiles/master/zsh/.zshrc > $HOME/.zshrc
+curl -sfL https://raw.githubusercontent.com/Appadeia/dotfiles/master/zsh/.zsh_plugins > $HOME/.zsh_plugins
+
+echo "${MAGENTA}Successfully installed fish-like zsh!${CLEAR}"
